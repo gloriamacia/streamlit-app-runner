f7baf4cacefb2848733d490f00f577ceafac7edd
Gloria Macia <gloriamaciamunoz@gmail.com>
2025-10-13 16:02:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,36 @@
 import streamlit as st
-import py3Dmol
-from stmol import showmol
-import biotite.structure.io.pdb as pdb
-import requests
+import pandas as pd
+import numpy as np
 
-# Set page config for better mobile display
-st.set_page_config(
-    page_title="PDB Viewer",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+st.title('Uber pickups in NYC')
 
-# Initialize session state
-if 'pdb_str' not in st.session_state:
-    st.session_state.pdb_str = None
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-def fetch_pdb(pdb_id):
-    url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    return None
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-def render_mol(pdb_str, style="cartoon", color_option="default", custom_color=None):
-    view = py3Dmol.view(width=1000, height=1000)  # Back to original fixed dimensions
-    view.addModel(pdb_str, "pdb")
-    
-    # Apply style and color
-    if color_option == "default":
-        view.setStyle({style: {'color': 'spectrum'}})
-    else:
-        view.setStyle({style: {'color': custom_color}})
-    
-    view.zoomTo()
-    showmol(view, height=1000, width=1000)  # Back to original fixed dimensions
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
 
-st.title("🧬 Simple PDB Viewer")
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
-# Sidebar styling options
-with st.sidebar:
-    st.markdown("## Style Options")
-    style = st.selectbox(
-        "Select Style",
-        ["cartoon", "line", "cross", "stick", "sphere"],
-        index=0
-    )
-    
-    color_option = st.radio(
-        "Color Option",
-        ["Default (Spectrum)", "Custom"],
-        index=0
-    )
-    
-    custom_color = None
-    if color_option == "Custom":
-        custom_color = st.color_picker("Select Color", "#FF0000")
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
-# Create two tabs for different input methods
-tab1, tab2 = st.tabs(["Upload PDB", "Enter PDB ID"])
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-with tab1:
-    uploaded_file = st.file_uploader("Upload a PDB file", type=['pdb'])
-    if uploaded_file is not None:
-        st.session_state.pdb_str = uploaded_file.getvalue().decode('utf-8')
-        render_mol(st.session_state.pdb_str, style, 
-                  "default" if color_option == "Default (Spectrum)" else "custom", 
-                  custom_color)
-
-with tab2:
-    pdb_id = st.text_input("Enter PDB ID (e.g., 1AKE)").strip()
-    if pdb_id:
-        pdb_str = fetch_pdb(pdb_id)
-        if pdb_str:
-            st.session_state.pdb_str = pdb_str
-            render_mol(st.session_state.pdb_str, style, 
-                      "default" if color_option == "Default (Spectrum)" else "custom", 
-                      custom_color)
-        else:
-            st.error("Unable to fetch PDB structure. Please check the ID.")
-
-# Instructions
-st.sidebar.markdown("""
-## Instructions
-1. Upload a PDB file or enter a PDB ID
-2. The structure will be displayed automatically
-3. Use mouse/touch to interact:
-   - Left click/one finger: rotate
-   - Right click/pinch: zoom
-   - Middle click/two fingers: translate
-""")
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
